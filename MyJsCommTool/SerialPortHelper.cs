f2321ef7d158b5bd78828ceb4a7176af283d5642
zhupengfei <zhupengfeivip@163.com>
2021-03-29 11:32:19 +0800

    完善串口发送

--- MyJsCommTool/SerialPortHelper.cs
+++ MyJsCommTool/SerialPortHelper.cs
@@ -18,43 +18,88 @@ namespace MyJsCommTool
     {
         public System.IO.Ports.SerialPort com = new System.IO.Ports.SerialPort();
 
-        /// <summary>
-        /// 天平通讯串口
-        /// </summary>
-        public string PortName = "COM3";
+        ///// <summary>
+        ///// 天平通讯串口
+        ///// </summary>
+        //public string PortName = "COM3";
+
+        ///// <summary>
+        ///// 波特率
+        ///// </summary>
+        //public int BaudRate = 57600;
+
+        ///// <summary>
+        ///// 数据位
+        ///// </summary>
+        //public int DataBits = 8;
+
+        ///// <summary>
+        ///// 停止位
+        ///// </summary>
+        //public int StopBits = (int)System.IO.Ports.StopBits.One;
+
+        ///// <summary>
+        ///// 校验位
+        ///// </summary>
+        //public int Parity = (int)System.IO.Ports.Parity.None;
+
 
         /// <summary>
-        /// 波特率
+        /// 
         /// </summary>
-        public int BaudRate = 57600;
-
-        /// <summary>
-        /// 数据位
-        /// </summary>
-        public int DataBits = 8;
-
-        /// <summary>
-        /// 停止位
-        /// </summary>
-        public int StopBits = (int)System.IO.Ports.StopBits.One;
-
-        /// <summary>
-        /// 校验位
-        /// </summary>
-        public int Parity = (int)System.IO.Ports.Parity.None;
-
-
-        public void init()
+        /// <param name="portName"></param>
+        /// <param name="baudRate"></param>
+        /// <param name="dataBits"></param>
+        /// <param name="stopBits"></param>
+        /// <param name="parity"></param>
+        public void init(string portName = "COM1", int baudRate = 57600, int dataBits = 8, int stopBits = 1, int parity = 0)
         {
-            com.PortName = "COM2";  //端口名称，默认COM1
-            com.BaudRate = BaudRate;
-            com.DataBits = DataBits;
-            com.StopBits = (System.IO.Ports.StopBits)StopBits;
-            com.Parity = (System.IO.Ports.Parity)Parity;
+            com.PortName = portName;  //端口名称，默认COM1
+            com.BaudRate = baudRate;
+            com.DataBits = dataBits;
+            com.StopBits = (System.IO.Ports.StopBits)stopBits;
+            com.Parity = (System.IO.Ports.Parity)parity;
 
-            com.Open();
+            Debug.WriteLine($"init data port:{portName} baudRate:{baudRate} dataBits:{dataBits} stopBits:{stopBits} parity:{parity}");
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        public void open()
+        {
+            try
+            {
+                com.Open();
+                Debug.WriteLine($"Open串口成功");
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine($"Open串口时发生异常," + ex);
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public void close()
+        {
+            try
+            {
+                com.Close();
+                Debug.WriteLine($"Close串口成功");
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine($"Close串口时发生异常," + ex);
+            }
+          
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
         public string recvData()
         {
             //Debug.WriteLine($"recvData successfully.");
@@ -71,20 +116,24 @@ namespace MyJsCommTool
             return buff.ByteToHexString();
         }
 
-        public void send_data(string data)
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="data"></param>
+        public void sendData(string data)
         {
+            if(com.IsOpen == false)
+            {
+                Debug.WriteLine($"串口未打开");
+                return;
+            }
+
             byte[] sendBytes = data.Replace(" ", "").ToBytes();
             com.Write(sendBytes, 0, sendBytes.Length);
 
             Debug.WriteLine($"send_data {data}");
         }
 
-        public int div(int dividend, int divisor)
-        {
-            Thread.Sleep(1000 * 2);
-            Debug.WriteLine($"{DateTime.Now} Object was bound successfully.");
-            return dividend / divisor;
-        }
 
     }
 }
