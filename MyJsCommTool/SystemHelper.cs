94ffb3b8eac5ed634f72c29c89e0c1625453a342
zhupengfei <zhupengfeivip@163.com>
2021-04-09 19:17:03 +0800

    增加一个示例

--- MyJsCommTool/SystemHelper.cs
+++ MyJsCommTool/SystemHelper.cs
@@ -32,6 +32,7 @@ namespace MyJsCommTool
                 {
                     case "当前时间":
                         {
+                            //参数格式 [当前时间,yyyyMMdd]
                             //第二个参数为时间格式
                             string dateFormat = cmdParas[1];
                             //将参数部分替换为要求的格式
@@ -40,7 +41,7 @@ namespace MyJsCommTool
                         break;
                     case "总加和":
                         {
-                            //第二个总加和从第几个字节开始，第三个参数为结束
+                            //参数格式 [总加和,2,8]第二个总加和从第几个字节开始，第三个参数为结束
                             //注意这里的位数是两位计算的，一个字节是两位
                             int startIndex = int.Parse(cmdParas[1]);
                             int stopIndex = int.Parse(cmdParas[2]);
@@ -52,7 +53,30 @@ namespace MyJsCommTool
                         break;
                     case "随机数字":
                         {
+                            //参数格式 [随机数字,0,100]
+                            //第二个参数表示最小值
+                            int minValue = int.Parse(cmdParas[1]);
+                            //第三个参数表示最大值
+                            int maxValue = int.Parse(cmdParas[2]);
+                            //第四个参数表示输出几位，就是几个字符，一般为双数
+                            int charCoount = int.Parse(cmdParas[3]);
+                            //第五个参数，输出格式1为16进制，默认；2为BCD
+                            int outType = 1;
+                            if(cmdParas.Length >= 5)
+                                outType = int.Parse(cmdParas[4]);
+                            Random rd = new Random();
+                            int rdValue = rd.Next(minValue, maxValue);
+                            string str;
+                            if (outType == 1)
+                                str = rdValue.ToString("X2");
+                            else
+                                str = rdValue.ToString();
 
+                            if (str.Length < charCoount)
+                                str = str.PadLeft(charCoount, '0');
+
+                            //将参数部分替换为要求的格式
+                            newData = newData.Replace(cmdText, str);
                         }
                         break;
                     default:
