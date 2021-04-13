fbaa5946eb07dafb8dadd08ff8f0935677114520
zhupengfei <zhupengfeivip@163.com>
2021-04-13 08:44:27 +0800

    清理无用代码，增加注释

--- MyJsCommTool/SerialPortHelper.cs
+++ MyJsCommTool/SerialPortHelper.cs
@@ -1,12 +1,7 @@
 ﻿using Bonn.Helper;
-using Newtonsoft.Json;
 using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
-using System.Text;
 using System.Threading;
-using System.Threading.Tasks;
 
 
 namespace MyJsCommTool
--- MyJsCommTool/SystemHelper.cs
+++ MyJsCommTool/SystemHelper.cs
@@ -1,10 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Net.Sockets;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Bonn.Helper;
 using System.Diagnostics;
 
